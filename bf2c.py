c4439bd77fd907c0e8dd93d88726161059ae2f8c
Matej Stuchlik <mstuchli@redhat.com>
2013-07-17 21:48:45 +0200

    Bunch of small changes

--- bf2c.py
+++ bf2c.py
@@ -10,11 +10,12 @@ bf2c = {">": "INCD()",  #increment data pointer \
         "[": "JUMPF()", #jump forward to ] if zero \
         "]": "JUMPB()"} #jump backward to [ if nonzero
 
+#We should check if [] are balanced
 with open(sys.argv[1]) as src, open("bf.c", "w") as outp:
     for n, line in enumerate(src):
-        outp.write("#line " + str(n) + '"%s"\n'%sys.argv[1])
+        outp.write("#line " + str(n) + '" %s"\n'%sys.argv[1])
         for l in line:
             if l in bf2c:
                 outp.write(bf2c[l] + "\n")
 
-subprocess.call(["gcc", "-g", "prelude.c", "-o", "bf"])
+subprocess.call(["gcc", "-g", "bf_runtime.c", "-o", "bf"])
